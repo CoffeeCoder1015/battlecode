c5a17a7f80110a4a19da7cb9ccd3bf550f388ba2
CoffeeCoder1015 <heroicnathan@gmail.com>
2025-01-10 22:58:02 +1300

    splasher init

--- java/src/myplayer/RobotPlayer.java
+++ java/src/myplayer/RobotPlayer.java
@@ -35,43 +35,24 @@ public class RobotPlayer {
      **/
     @SuppressWarnings("unused")
     public static void run(RobotController rc) throws GameActionException {
-        // Hello world! Standard output is very useful for debugging.
-        // Everything you say here will be directly viewable in your terminal when you run a match!
-        System.out.println("I'm alive");
 
-        // You can also use indicators to save debug notes in replays.
         rc.setIndicatorString("Hello world!");
 
         while (true) {
-            // This code runs during the entire lifespan of the robot, which is why it is in an infinite
-            // loop. If we ever leave this loop and return from run(), the robot dies! At the end of the
-            // loop, we call Clock.yield(), signifying that we've done everything we want to do.
+            turnCount += 1;  
 
-            turnCount += 1;  // We have now been alive for one more turn!
-
-            // Try/catch blocks stop unhandled exceptions, which cause your robot to explode.
             try {
-                // The same run() function is called for every robot on your team, even if they are
-                // different types. Here, we separate the control depending on the UnitType, so we can
-                // use different strategies on different robots. If you wish, you are free to rewrite
-                // this into a different control structure!
                 switch (rc.getType()){
                     case SOLDIER: runSoldier(rc); break; 
                     case MOPPER: runMopper(rc); break;
-                    case SPLASHER: break; // Consider upgrading examplefuncsplayer to use splashers!
+                    case SPLASHER: runSplasher(rc); break; // Consider upgrading examplefuncsplayer to use splashers!
                     default: runTower(rc); break;
-                    }
                 }
-             catch (GameActionException e) {
-                // Oh no! It looks like we did something illegal in the Battlecode world. You should
-                // handle GameActionExceptions judiciously, in case unexpected events occur in the game
-                // world. Remember, uncaught exceptions cause your robot to explode!
+            }
+            catch (GameActionException e) {
                 System.out.println("GameActionException");
                 e.printStackTrace();
-
             } catch (Exception e) {
-                // Oh no! It looks like our code tried to do something bad. This isn't a
-                // GameActionException, so it's more likely to be a bug in our code.
                 System.out.println("Exception");
                 e.printStackTrace();
 
@@ -80,10 +61,11 @@ public class RobotPlayer {
                 // This will make our code wait until the next turn, and then perform this loop again.
                 Clock.yield();
             }
-            // End of loop: go back to the top. Clock.yield() has ended, so it's time for another turn!
         }
+    }
 
-        // Your code should never reach here (unless it's intentional)! Self-destruction imminent...
+    public static void runSplasher(RobotController rc) {
+        rc.setIndicatorString("I do nothing.");
     }
 
     /**
