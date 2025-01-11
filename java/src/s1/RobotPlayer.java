3204a9b1f1478e70c401f5aeb6eb485f828e9b19
CoffeeCoder1015 <heroicnathan@gmail.com>
2025-01-12 08:53:14 +1300

    Self implementation 1 (s1)

---
+++ java/src/s1/RobotPlayer.java
@@ -0,0 +1,21 @@
+package s1;
+
+import battlecode.common.*;
+
+public class RobotPlayer {
+
+    static final Direction[] directions = {
+            Direction.NORTH,
+            Direction.NORTHEAST,
+            Direction.EAST,
+            Direction.SOUTHEAST,
+            Direction.SOUTH,
+            Direction.SOUTHWEST,
+            Direction.WEST,
+            Direction.NORTHWEST,
+    };
+
+    public static void run(RobotController rc) throws GameActionException {
+
+    }
+}
