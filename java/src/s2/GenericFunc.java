0de1466e7aad85f9362ca1bba0c4ca3a1c9e15b8
CoffeeCoder1015 <heroicnathan@gmail.com>
2025-01-19 12:27:16 +1300

    RCFunc reworked it is now GenericFunc. RobotController is passed in through the class

---
+++ java/src/s2/GenericFunc.java
@@ -0,0 +1,8 @@
+package s2;
+
+import battlecode.common.*;
+
+@FunctionalInterface
+public interface GenericFunc {
+    void p() throws GameActionException;
+}
--- java/src/s2/RCFunc.java
+++
@@ -1,8 +0,0 @@
-package s2;
-
-import battlecode.common.*;
-
-@FunctionalInterface
-public interface RCFunc {
-    void p(RobotController r) throws GameActionException;
-}
