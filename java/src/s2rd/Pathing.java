75df7b75c598f6799456a876b3a6470a4a65d121
CoffeeCoder1015 <heroicnathan@gmail.com>
2025-01-20 19:30:40 +1300

    Obtaining Initial direction

--- java/src/s2rd/Pathing.java
+++ java/src/s2rd/Pathing.java
@@ -6,7 +6,7 @@ import s2.generics.GenericFunc;
 import java.util.Random;
 
 public class Pathing {
-    static final Direction[] directions = {
+    final Direction[] directions = {
             Direction.NORTH,
             Direction.NORTHEAST,
             Direction.EAST,
@@ -16,24 +16,59 @@ public class Pathing {
             Direction.WEST,
             Direction.NORTHWEST,
     };
+    // boids
+    // rule
+    // 1. Steer away from nearby robots
+    // 2. align direction with nearby robots
+    // 3. Steer towards the center of mass * (I dont think this is good for our case)
 
 
-    static final Random rng = new Random(6147);
-
-    static int robot_dir_idx = -1; //also technically velocity
+    // diffusion core
+    // 1. Each particle must have a energy
+    // 2. Particles glide away from nearby robots
+    // 3. Particles lose energy from collsion 
     
-    static RobotController rc;
+    // Robots receieve an initial energy and direction from their spawn tower 
+    // This ensures the robots are in a field pushing them away from the tower
+    // When this energy reaches 0 as it collides with objects we hand back the control to the robot itself 
 
-    static private int modulo(int x, int y) {
-        int temp = Math.floorDiv(x,y);
-        return x-temp*y;
-    }
+
+    final Random rng = new Random(6147);
+
+    int robot_dir_idx = -1; //also technically velocity
+    int energy = 35;
+    
+    RobotController rc;
 
     public Pathing(RobotController handler) throws GameActionException{
         rc = handler;
+        getInitalDir();
     }
 
     public void Move() throws GameActionException{
     }
+
+    private int modulo(int x, int y) {
+        int temp = Math.floorDiv(x,y);
+        return x-temp*y;
+    }
+
+    private void getInitalDir() throws GameActionException{
+        MapInfo[] infos = rc.senseNearbyMapInfos(2);
+        MapLocation closestTower = null;
+        for (MapInfo mapInfo : infos) {
+            if (mapInfo.hasRuin()) {
+               closestTower = mapInfo.getMapLocation(); 
+            }
+        }
+        Direction InitalDirection  =closestTower.directionTo(rc.getLocation());
+        for (int i = 0; i < 8; i++) {
+           if (directions[i] == InitalDirection) {
+                robot_dir_idx = i; 
+                break;
+           } 
+        }
+    }
+
 
 }
