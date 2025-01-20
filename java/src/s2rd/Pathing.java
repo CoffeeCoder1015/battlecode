6d06add4c8425a7c9b1b8159b40c00610f0d92a8
CoffeeCoder1015 <heroicnathan@gmail.com>
2025-01-21 04:19:27 +1300

    better diffusion implemented, idk why they still hug the wall yet

--- java/src/s2rd/Pathing.java
+++ java/src/s2rd/Pathing.java
@@ -1,12 +1,11 @@
 package s2rd;
 
 import battlecode.common.*;
-import s2rd.generics.GenericFunc;
 
 import java.util.Random;
 
 public class Pathing {
-    static final Direction[] directions = {
+    final Direction[] directions = {
             Direction.NORTH,
             Direction.NORTHEAST,
             Direction.EAST,
@@ -31,24 +30,29 @@ public class Pathing {
     // Robots receieve an initial energy and direction from their spawn tower 
     // This ensures the robots are in a field pushing them away from the tower
     // When this energy reaches 0 as it collides with objects we hand back the control to the robot itself 
-    
-    GenericFunc movement;
 
     final Random rng = new Random(6147);
 
-    static int robot_dir_idx = -1; //also technically velocity
-    static int energy = 35;
+    int robot_dir_idx = -1; //also technically velocity
+    int energy = 40;
     
-    static RobotController rc;
+    RobotController rc;
+    
+    MapLocation currentLocation;
+    
+    RobotInfo[] rbInfo;
+    MapInfo[] infos;
 
     public Pathing(RobotController handler) throws GameActionException{
         rc = handler;
-        movement = Pathing::Diffuse;
         getInitalDir();
     }
 
     public void Move() throws GameActionException{
-        movement.p();
+        rbInfo = rc.senseNearbyRobots(-1);
+        infos = rc.senseNearbyMapInfos(8);
+        currentLocation = rc.getLocation();
+        Diffuse();
     }
 
     private int modulo(int x, int y) {
@@ -64,33 +68,112 @@ public class Pathing {
                closestTower = mapInfo.getMapLocation(); 
             }
         }
-        Direction InitalDirection  =closestTower.directionTo(rc.getLocation());
-        for (int i = 0; i < 8; i++) {
-           if (directions[i] == InitalDirection) {
-                robot_dir_idx = i; 
-                break;
-           } 
+        if (closestTower == null) {
+           return; 
         }
+        Direction InitialDirection  =closestTower.directionTo(rc.getLocation());
+        robot_dir_idx = DirToIndex(InitialDirection);
+    }
+    
+    private int DirToIndex(Direction dir) {
+        int x = Math.abs(dir.dx);
+        int y = Math.abs(dir.dy);
+        int idx = x*(3-(x+y));
+        if (dir.dx < 0 || dir.dy < 0) {
+            idx+=4; 
+        }
+        return idx;
     }
 
-    static private void Diffuse() throws GameActionException{
-
+    private void Diffuse() throws GameActionException{
         Direction goalDirection = directions[robot_dir_idx];
         MapLocation currentLocation = rc.getLocation();
-        MapLocation targLocation = currentLocation.add(goalDirection);
+        MapLocation targetLocation = currentLocation.add(goalDirection);
         boolean canMove =rc.canMove(goalDirection);
+
         if (canMove) {
-           rc.move(goalDirection); 
+            rc.move(goalDirection); 
             return;
         }
+    
+        calculateNextIdx(targetLocation);        
+    }
+    private void calculateNextIdx(MapLocation targetLocation) throws GameActionException{
         // collision has occurred
         // if wall or ruin then energy decrease to 0
         // else decrease by 1
-        MapInfo infos = rc.senseMapInfo(targLocation);
-        if (infos.isPassable()) {
-            energy--;
-        } else {
-            energy = 0;
+        boolean isWallLike = false;
+        if (rc.canSenseLocation(targetLocation)) {
+            MapInfo infos = rc.senseMapInfo(targetLocation);
+            isWallLike = !infos.isPassable();
         }
+        if (getDirRobotCount() > 13) {
+            isWallLike = true;
+        }
+        if (treatAllyTileAsWall()) {
+            isWallLike = true;
+        }
+        if (isWallLike) {
+            energy = 0;
+        } else {
+            energy--;
+        }
+        switch (energy) {
+            case 0: // wall or ruin collision
+                // bounce back
+                energy = rng.nextInt(15) + 20;
+                robot_dir_idx = modulo(robot_dir_idx + 3 + rng.nextInt(3), 8);
+                break;
+            default: // other robot collision
+                // dodge
+                int decide = rng.nextInt(10);
+                if (decide < 4) {
+                    if (decide <= 1) {
+                        robot_dir_idx = modulo(robot_dir_idx + 1, 8);
+                    } else{
+                       robot_dir_idx = modulo(robot_dir_idx - 1, 8);
+                   }
+                }
+                break;
+        }       
+    }
+    
+    private int getDirRobotCount() throws GameActionException{
+        int rbCount = 0;
+        for (RobotInfo rb: rbInfo) {
+            if (rb.getType().isTowerType()) {
+               continue; 
+            }
+            MapLocation loc = rb.getLocation();
+            Direction dirTo = rc.getLocation().directionTo(loc);
+            int idx  = DirToIndex(dirTo);
+            int idx_dist_to_current = modulo(robot_dir_idx - idx, 8);
+            if(0<=idx_dist_to_current && idx_dist_to_current<=2){
+                // rc.setIndicatorLine( loc,currentLocation, 23,177,23);
+                rbCount++;
+            }
+        }
+        return rbCount;
+    }
+    
+    private boolean treatAllyTileAsWall() throws GameActionException{
+        int infront = 0;
+        int behind = 0;
+        for (MapInfo mapInfo: infos) {
+            if (!mapInfo.getPaint().isAlly()) {
+               continue; 
+            }
+            MapLocation loc = mapInfo.getMapLocation();
+            Direction dirTo = rc.getLocation().directionTo(loc);
+            int idx  = DirToIndex(dirTo);
+            int idx_dist_to_current = modulo(robot_dir_idx - idx, 8);
+            if(0<=idx_dist_to_current && idx_dist_to_current<=2){
+                // rc.setIndicatorLine( loc,currentLocation, 120,188,233);
+                infront++;
+            }else{
+                behind++;
+            }
+        }
+        return infront>behind;
     }
 }
